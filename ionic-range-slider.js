15d2a31f2bb6baf7943736e73dd1c36ce539017e
Geoffrey Bauduin <bauduin.geo@gmail.com>
2016-02-07 15:57:35 +0100

    init

---
+++ ionic-range-slider.js
@@ -0,0 +1,77 @@
+/**
+ * @Author: Geoffrey Bauduin <bauduin.geo@gmail.com>
+ */
+
+angular.module("ion.rangeslider", []);
+
+angular.module("ion.rangeslider").directive("ionRangeSlider", [
+    function () {
+        
+        return {
+            restrict: "E",
+            scope: {
+                min: "=",
+                max: "=",
+                type: "@",
+                prefix: "@",
+                maxPostfix: "@",
+                prettify: "@",
+                grid: "@",
+                gridMargin: "@",
+                postfix: "@",
+                step: "@",
+                hideMinMax: "@",
+                hideFromTo: "@",
+                from: "=",
+                to: "=",
+                disable: "=",
+                onChange: "=",
+                onFinish: "="
+            },
+            replace: true,
+            link: function ($scope, $element) {
+                $element.ionRangeSlider({
+                    min: $scope.min,
+                    max: $scope.max,
+                    type: $scope.type,
+                    prefix: $scope.prefix,
+                    maxPostfix: $scope.maxPostfix,
+                    prettify: $scope.prettify,
+                    grid: $scope.grid,
+                    gridMargin: $scope.gridMargin,
+                    postfix: $scope.postfix,
+                    step: $scope.step,
+                    hideMinMax: $scope.hideMinMax,
+                    hideFromTo: $scope.hideFromTo,
+                    from: $scope.from,
+                    to: $scope.to,
+                    disable: $scope.disable,
+                    onChange: $scope.onChange,
+                    onFinish: $scope.onFinish
+                });
+                var watchers = [];
+                watchers.push($scope.$watch("min", function (value) {
+                    $element.data("ionRangeSlider").update({
+                        min: value
+                    });
+                }));
+                watchers.push($scope.$watch('max', function (value) {
+                    $element.data("ionRangeSlider").update({
+                        max: value
+                    });
+                }));
+                watchers.push($scope.$watch('from', function (value) {
+                    $element.data("ionRangeSlider").update({
+                        from: value
+                    });
+                }));
+                watchers.push($scope.$watch('disable', function (value) {
+                    $element.data("ionRangeSlider").update({
+                        disable: value
+                    });
+                }));
+            }
+        }
+        
+    }
+])
